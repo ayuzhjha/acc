c46399a614d6c401331f5c31c6fa23daa948d4ab
Ayush Jha <ayushjha949@gmail.com>
2025-12-21 10:06:27 +0530

    fix backend url

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,12 @@
-export const getApiUrl = () => {
-    // if (process.env.NEXT_PUBLIC_API_URL) {
-    //     return process.env.NEXT_PUBLIC_API_URL;
-    // }
+if (process.env.NEXT_PUBLIC_API_URL) {
+    return process.env.NEXT_PUBLIC_API_URL;
+}
 
-    // if (process.env.NODE_ENV === 'production') {
-    //     return 'https://acm-backend-u2ng.onrender.com';
-    // }
+if (process.env.NODE_ENV === 'production') {
+    return 'https://acm-backend-u2ng.onrender.com';
+}
 
-    return 'http://localhost:5000';
+return 'http://localhost:5000';
 };
 
 export const API_URL = getApiUrl();
